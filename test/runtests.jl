b19214b2011e247ea35942e985740a46991f4654
Alexander Barth <barth.alexander@gmail.com>
2020-12-09 20:18:33 +0100

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,14 @@
 using Test
 using DIVAnd_hfradar
 
-include("test_DIVAndrun.jl")
+@testset "adjoint" begin
+    include("test_adjoint.jl")
+end
+
+@testset "adjoint misfit" begin
+    include("test_adjoint_misfit.jl")
+end
+
+@testset "DIVAndrun analysis" begin
+    include("test_DIVAndrun.jl")
+end
